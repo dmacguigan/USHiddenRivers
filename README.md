8d6872ecfeb09568468b19ebc5833f6a3ef50080
dmacguigan <dmacguig@buffalo.edu>
2021-03-17 16:26:23 -0400

    new rivers

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 # Hidden Rivers of the United States
-Scripts to plot river systems across the United States. Requires downloading several of large shapefiles. Data source info can be found in the script.
+Scripts to plot river systems across the United States. Requires a number several of large shapefiles, available for download from [Google drive](https://drive.google.com/drive/folders/1kcnSb0gbX7t25eKa3k3dVA84PbOYdz2L?usp=sharing)
 
 Follow the series on [Twitter @DMacGuig](https://twitter.com/DMacGuig)
 
 ## Examples
-![St. Johns River](./maps/StJohns.png)
+![#1 St. Johns River](./maps/StJohns.png)
+Drainage: 24,516 km² (9,465 mi²)
 
-![St. Marys River](./maps/StMarys.png)
+![#2 St. Marys River](./maps/StMarys.png)
+Drainage: 4,101 km² (1,583 mi²)
 
-![Satilla River](./maps/Satilla.png)
+![#3 Satilla River](./maps/Satilla.png)
+Drainage: 8,752 km² (3,379 mi²)
 
-![Altamaha River](./maps/Altamaha.png)
+![#4 Altamaha River](./maps/Altamaha.png)
+Drainage: 37,251 km² (14,383 mi²)
 
-![Ogeechee River](./maps/Ogeechee.png)
+![#5 Ogeechee River](./maps/Ogeechee.png) 
+Drainage: 12,317 km² (4,756 mi²)
 
+![# 6 Savannah River](./maps/Savannah.png)
+Drainage: 28,195 km² (10,886 mi²)
+
+![# 7 Chehalis River](./maps/Chehalis_crop.png)
+Drainage: 7,002 km² (2,703 mi²)
+
+![# 8 Combahee River](./maps/Combahee_crop.png)
+Drainage: 4,758 km² (1,837 mi²)
+
+![# 9 Edisto River](./maps/Edisto_crop.png)
+Drainage: 2,498 km² (6,470 mi²)
---
+++ scripts/riverMap_7_Chehalis.R
@@ -0,0 +1,283 @@
+##############################################################################################################################
+# HIDDEN RIVERS of the UNITED STATES
+# script to plot rivers, waterbodies, and watersheds
+# author: Dan MacGuigan
+# dmacguig@buffalo.edu
+
+##############################################################################################################################
+
+library(RColorBrewer)
+library(maps)
+library(rgdal)
+library(gridExtra)
+library(maptools)
+library(scales)
+library(raster)
+library(rgeos)
+library(ggplot2)
+library(grid)
+library(mapplots)
+library(dplyr)    
+library(mapproj)
+library(ggmap)
+library(ggspatial)
+library(sf)
+library(ggrepel)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rnaturalearthhires)
+library(cowplot)
+
+##############################################################################################################################
+# specify the following parameters
+HUCs <- c("17100105", "17100104", "17100103") # top-level vector of HUCs to match (must be strings, not numeric)
+# search for HUCs here: https://water.usgs.gov/wsc/map_index.html
+states <- c("WA") # states spanned by the river basin (for parsing waterbodies), use abbreviations
+river_name <- "Chehalis" # river name for files
+river_name_plot <- "Chehalis" # river name for plot title
+wd <- "H:/USHiddenRivers/" # top level working directory
+
+setwd(wd)
+
+##############################################################################################################################
+# READ DATA and PARSE shapefiles
+
+# BASINS
+# load shapefile for US watersheds (HUC 10 level)
+# data from https://www.usgs.gov/core-science-systems/ngp/national-hydrography/access-national-hydrography-products
+# will need to use external GIS platform such as QGIS to extract HUC 10 watersheds from database
+watersheds <- readOGR(paste(wd, "/shapefiles/watersheds/WBD_HU10_watersheds.shp", sep=""))
+watersheds <- spTransform(watersheds, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+huc10s <- as.character(watersheds$huc10)
+target_watershed <- watersheds[which(as.logical(rowSums(sapply(HUCs, startsWith, x=huc10s)))),]
+target_watershed <- aggregate(target_watershed, dissolve=T)
+# crop watershed at coastline using low resolution state borders shapefile
+# data from https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_state_5m.zip
+state_borders_lowres <- readOGR(paste(wd, "/shapefiles/stateBorders_lowRes/cb_2018_us_division_5m.shp", sep=""))
+target_watershed <- crop(target_watershed, state_borders_lowres)
+target_watershed <- as(target_watershed, "SpatialPolygonsDataFrame" )
+setwd(paste(wd, "./shapefiles/isolatedWatersheds", sep=""))
+writeOGR(target_watershed, dsn = '.', layer = paste(river_name, "_watershed", sep=""), driver = "ESRI Shapefile", overwrite_layer=TRUE)
+
+# RIVERS
+# load shapefile for all US streams at 1 million scale
+# data from https://nationalmap.gov/small_scale/mld/1strmsl.html
+rivers <- readOGR(paste(wd, "/shapefiles/allRivers/streaml010g.shp", sep=""))
+rivers <- spTransform(rivers, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+#reachCodes <- as.character(rivers$ReachCode)
+#target_river <- rivers[which(as.logical(rowSums(sapply(HUCs, startsWith, x=reachCodes)))),]
+#target_river <- as(target_river, "SpatialLinesDataFrame" )
+# crop rivers by watershed boundary
+in_ws_rivers <- over(rivers, target_watershed)
+target_rivers_ws <- rivers[!is.na(in_ws_rivers$dummy),]
+# remove intercoastal waterway
+feat <- as.character(target_rivers_ws@data$Feature)
+target_rivers_ws <- target_rivers_ws[which(!as.logical(rowSums(sapply(c("Intracoastal Waterway"), grepl, x=feat, fixed=TRUE)))),]
+setwd(paste(wd, "./shapefiles/isolatedRivers", sep=""))
+writeOGR(target_rivers_ws, dsn = '.', layer = paste(river_name, "_river", sep=""), driver = "ESRI Shapefile", overwrite_layer=TRUE)
+
+# WATERBODIES
+# read shapefile
+# data from https://maps.princeton.edu/catalog/stanford-sv709xw7113
+wbs <- readOGR(paste(wd, "/shapefiles/waterbodies/wtrbdyp010g.shp", sep=""))
+wbs <- spTransform(wbs, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+state <- as.character(wbs$State)
+target_wbs <- wbs[which(as.logical(rowSums(sapply(states, grepl, x=state, fixed=TRUE)))),]
+feature <- as.character(target_wbs$Feature)
+# lakes only
+target_wbs_lakes <- target_wbs[which(as.logical(rowSums(sapply(c("Lake"), grepl, x=feature, fixed=TRUE)))),]
+in_ws_lakes <- over(target_wbs_lakes, target_watershed)
+target_wbs_lakes <- target_wbs_lakes[!is.na(in_ws_lakes$dummy),]
+target_wbs_lakes <- aggregate(target_wbs_lakes, dissolve=T)
+target_wbs_lakes <- as(target_wbs_lakes, "SpatialPolygonsDataFrame" )
+setwd(paste(wd, "/shapefiles/isolatedWaterbodies", sep=""))
+writeOGR(target_wbs_lakes, dsn = '.', layer = paste(river_name, "_waterbodies_lakes", sep=""), driver = "ESRI Shapefile", overwrite_layer=TRUE)
+# swamps only
+target_wbs_swamps <- target_wbs[which(as.logical(rowSums(sapply(c("Swamp or Marsh"), grepl, x=feature, fixed=TRUE)))),]
+in_ws_swamps <- over(target_wbs_swamps, target_watershed)
+target_wbs_swamps <- target_wbs_swamps[!is.na(in_ws_swamps$dummy),]
+target_wbs_swamps <- aggregate(target_wbs_swamps, dissolve=T)
+target_wbs_swamps <- as(target_wbs_swamps, "SpatialPolygonsDataFrame")
+setwd(paste(wd, "/shapefiles/isolatedWaterbodies", sep=""))
+writeOGR(target_wbs_swamps, dsn = '.', layer = paste(river_name, "_waterbodies_swamps", sep=""), driver = "ESRI Shapefile", overwrite_layer=TRUE)
+
+# STATE BORDERS
+# read in shape border shapefile
+# data from https://github.com/jasperdebie/VisInfo/blob/master/us-state-capitals.csv
+state_borders <- readOGR(paste(wd, "/shapefiles/stateBorders/statesp010g.shp", sep=""))
+state_borders <- spTransform(state_borders, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# INLETS
+inlet_outline <- crop(state_borders, target_watershed)
+inlet <- gDifference(target_watershed, inlet_outline)
+inlet <- as(inlet, "SpatialPolygonsDataFrame" )
+setwd(paste(wd, "/shapefiles/isolatedInlets", sep=""))
+writeOGR(inlet, dsn = '.', layer = paste(river_name, "_inlet", sep=""), driver = "ESRI Shapefile", overwrite_layer=TRUE)
+
+# COASTLINE
+# data from https://www.sciencebase.gov/catalog/item/581d051ce4b08da350d523ba
+coastline <- readOGR(paste(wd, "/shapefiles/coastline/coastll010g.shp", sep=""))
+coastline <- spTransform(coastline, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# STATE CAPITOLS
+# data from https://github.com/jasperdebie/VisInfo/blob/master/us-state-capitals.csv
+#capitols <- read.csv(paste(wd, "/data/stateCapitols.csv", sep=""), header=TRUE)
+
+# CITIES
+# data from https://github.com/jasperdebie/VisInfo/blob/master/us-state-capitals.csv
+# and https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population
+cities <- read.csv(paste(wd, "/data/majorCities.csv", sep=""), header=TRUE)
+
+
+# FALL LINE
+# data from http://www.ceus-ssc.org/Report/GIS.html
+fall_line <- readOGR(paste(wd, "/shapefiles/fallLine/CHAR_FallLine_Hibbard_etal_2006_R0.shp", sep=""))
+fall_line <- spTransform(fall_line, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# RASTER TOPO DATA 
+# from https://www.naturalearthdata.com/downloads/10m-raster-data/10m-shaded-relief/
+topo <- raster(paste(wd, "/shapefiles/shadedRelief/GRAY_HR_SR_OB.tif", sep=""))
+
+
+###############################################################################################################################
+# PLOT MAP
+
+# get extent of target watershed
+extents <- extent(target_watershed)
+# get x and y min and max values
+# may need to adjust these manually
+ymax <- ceiling(extents@ymax)
+extents@ymax <- ymax
+ymin <- floor(extents@ymin)
+extents@ymin <- ymin
+xmax <- ceiling(extents@xmax) + 0.5
+extents@xmax <-  xmax
+xmin <- floor(extents@xmin)
+extents@xmin <- xmin
+
+
+# set up data for ggplot
+world <- ne_countries(scale='large',returnclass = 'sf')
+us_states <- ne_states(country="United States of America", returnclass = 'sf')
+capitols_subset <- capitols[which(capitols$lat < ymax & capitols$lat > ymin &
+                                    capitols$long < xmax & capitols$long > xmin),]
+cities_subset <- cities[which(cities$lat < ymax & cities$lat > ymin &
+                                  cities$long < xmax & cities$long > xmin),]
+cities_subset <- cities_subset[-1,] 
+inlet_sf <- st_as_sf(inlet)
+#inlet_sf_crop <- st_crop(inlet_sf, xmin = xmin, xmax = xmax,
+#                         ymin = ymin, ymax = ymax)
+state_borders_sf <- st_as_sf(state_borders)
+state_borders_sf_crop <- st_crop(state_borders_sf, xmin = xmin, xmax = xmax,
+                                 ymin = ymin, ymax = ymax)
+coastline_sf <- st_as_sf(coastline)
+coastline_sf_crop <- st_crop(coastline_sf, xmin = xmin, xmax = xmax,
+                                 ymin = ymin, ymax = ymax)
+state_borders_sf <- st_as_sf(state_borders)
+state_borders_sf_crop <- st_crop(state_borders_sf, xmin = xmin, xmax = xmax,
+                                 ymin = ymin, ymax = ymax)
+target_wbs_swamps_sf <- st_as_sf(target_wbs_swamps)
+target_wbs_swamps_sf_crop <- st_crop(target_wbs_swamps_sf, xmin = xmin, xmax = xmax,
+                                 ymin = ymin, ymax = ymax)
+target_wbs_lakes_sf <- st_as_sf(target_wbs_lakes)
+target_wbs_lakes_sf_crop <- st_crop(target_wbs_lakes_sf, xmin = xmin, xmax = xmax,
+                                 ymin = ymin, ymax = ymax)
+target_watershed_sf <- st_as_sf(target_watershed)
+target_watershed_sf_crop <- st_crop(target_watershed_sf, xmin = xmin, xmax = xmax,
+                                    ymin = ymin, ymax = ymax)
+rivers_sf <- st_as_sf(rivers)
+rivers_sf_crop <- st_crop(rivers_sf, xmin = xmin, xmax = xmax,
+                                    ymin = ymin, ymax = ymax)
+fall_line_sf <- st_as_sf(fall_line)
+fall_line_sf_crop <- st_crop(fall_line_sf, xmin = xmin, xmax = xmax,
+                          ymin = ymin, ymax = ymax)
+target_rivers_ws_sf <- st_as_sf(target_rivers_ws)
+target_rivers_ws_sf_crop <- st_crop(target_rivers_ws_sf, xmin = xmin, xmax = xmax,
+                          ymin = ymin, ymax = ymax)
+topo_crop <- crop(topo, extents)
+topo_crop_df <- as.data.frame(topo_crop, xy = TRUE)
+
+# replace negative stream order values with 1
+rivers_sf_crop$Strahler[rivers_sf_crop$Strahler<0] <- 1
+target_rivers_ws_sf_crop$Strahler[target_rivers_ws_sf_crop$Strahler<0] <- 1
+
+# inset plot
+gworld <- ggplot(data = world) +
+  geom_sf(fill="gray20", color="white", size=0.3) +
+  geom_sf(data=us_states, fill="gray20", color="white", size=0.3) +
+  geom_rect(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, 
+            fill = NA, colour = "red", size = 0.8) +
+  coord_sf(xlim = c(-130, -65), ylim = c(25, 55), expand = FALSE, datum=NA) +
+  theme(panel.border = element_rect(colour = "gray60", fill=NA, size=1), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_blank(), 
+        panel.background = element_rect(fill = "#1c5163"),
+        plot.background = element_rect(fill = "transparent", color = NA))
+gworld_ratio <- (xmax - xmin) / (ymax - (ymin))
+
+
+# main plot
+p <- ggplot() +
+  geom_sf(data=state_borders_sf_crop, fill="black", color=NA) +
+  geom_raster(topo_crop_test, mapping = aes(x = x, y = y, alpha = GRAY_HR_SR_OB)) +
+  scale_alpha(range =  c(0, 1), guide = "none") + 
+  geom_sf(data=target_watershed_sf_crop, fill=alpha("white", 0.15), color=NA) +
+  geom_sf(data=rivers_sf_crop, color="#565656", lineend = "round", aes(size=factor(Strahler)), show.legend = FALSE,) +
+  #geom_sf(data=state_borders_sf_crop, fill=NA, color="white") +
+  #geom_sf(data=coastline_sf_crop, fill=NA, color="black") +
+  #geom_sf(data=fall_line_sf_crop, fill=NA, color="red", linetype="dashed") +
+  geom_sf(data=target_wbs_swamps_sf_crop, fill=alpha("palegreen", 0.3), color=NA) +
+  #geom_sf(data=inlet_sf, fill="#82eefd", color="#82eefd") +
+  geom_sf(data=target_wbs_lakes_sf_crop, fill="#82eefd", color=NA) +
+  geom_sf(data=target_rivers_ws_sf_crop, aes(size=factor(Strahler)), color="#82eefd", 
+          show.legend = FALSE, lineend = "round") +
+  geom_point(data = cities_subset, aes(y=lat, x=long), pch=21, size=1, stroke=2, color="white", fill="black") +
+  geom_label_repel(data = cities_subset, aes(y=lat, x=long, label=paste(city, ", ", state, sep="")),
+                   box.padding   = 0.35, 
+                   point.padding = 0.5,
+                   segment.color = 'white', show.legend = FALSE) + 
+  scale_size_manual(values=seq(0.2,2,by=0.1)) +
+  annotation_scale(location = "bl", width_hint = 0.2, unit_category="imperial", text_col="white", line_col="white") + # adjust scale location as needed
+  annotation_scale(location = "tl", width_hint = 0.2, unit_category="metric",  text_col="white", line_col="white") + # adjust scale location as needed
+  annotation_north_arrow(location = "bl", which_north = "true", # adjust arrow location as needed
+                         pad_x = unit(0.2, "in"), pad_y = unit(0.5, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  annotate("text", x=-80.5, y=34.77, label="South Carolina", color="white", hjust=0, size=4) +
+  annotate("text", x=-80.5, y=34.85, label="North Carolina", color="white", hjust=0, size=4) +
+  labs(title = paste(river_name_plot, "River Basin")) +
+  coord_equal() +
+  coord_sf(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = FALSE) + 
+  ylab("") +
+  xlab("") +
+  theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.5), 
+        panel.background = element_rect(fill = "#1c5163"),
+        plot.title = element_text(color = "black", size = 16, hjust = 0.5))
+
+# adjust x and y position and size of inset plot
+xpos = 0.25
+ypos = 0.55
+size = 0.16
+ggdraw(p) + 
+  draw_plot(gworld, width = size, height = size * 10/6 * gworld_ratio, 
+            x = xpos, y = ypos, hjust=0, vjust=0) 
+
+# plot dimensions
+# mess with the multipliers to get a nice figure
+height = (ymax-ymin)*2.8
+width = (xmax-xmin)*2.8
+# save as png
+ggsave(paste(wd, "maps/", river_name, ".png", sep=""), units = "in", dpi=600, height=height, width=width)
+ggsave(paste(wd, "maps/", river_name, ".pdf", sep=""), height=height, width=width)
+
+
+# calculate area of watershed square km
+raster::area(target_watershed) / 1000000 
+# [1] 7001.821
+
+# calculate area of watershed in square miles
+(raster::area(target_watershed) / 1000000) * 0.386102
+# [1] 2703.417
+
+# calculatae max stream order
+max(target_rivers_ws_sf_crop$Strahler)
+# [1] 4
+
